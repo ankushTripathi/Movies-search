f5ae952b2677d15ced90269a3713d2db58c3d0be
ankush tripathi <ankushtripathi0996@yahoo.com>
2017-05-11 18:54:17 +0530

    fix bugs

--- js/controller/MoviesController.js
+++ js/controller/MoviesController.js
@@ -5,7 +5,15 @@ app.controller('SearchList',function($scope,$http){
     $scope.results = {};
     $scope.errors = {};
     $scope.searchMovie = function(){
-            $http.get('http://www.omdbapi.com/?t='+$scope.movieSearched.title+'&y='+$scope.movieSearched.year)
+        var url;
+        if($scope.movieSearched.title===undefined && $scope.movieSearched.year === undefined)
+           $scope.errors = {"error": "Enter Movie Title or Year"}; 
+        else if($scope.movieSearched.title===undefined)
+            url = 'http://www.omdbapi.com/?y='+$scope.movieSearched.year;
+        else
+            url = 'http://www.omdbapi.com/?t='+$scope.movieSearched.title+'&y='+$scope.movieSearched.year;
+
+            $http.get(url)
                         .success(function(data,status,headers,config){
                             if(status === 200)
                              $scope.results = data;
