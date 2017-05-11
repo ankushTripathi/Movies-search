266656ef23207aa186592ab56d89f4b3dbd4e87e
ankush tripathi <ankushtripathi0996@yahoo.com>
2017-05-11 18:08:41 +0530

    search implemented

--- js/controller/MoviesController.js
+++ js/controller/MoviesController.js
@@ -2,13 +2,21 @@ app.controller('SearchList',function($scope,$http){
 
     $scope.movieSearched = {};
     
-
+    $scope.results = {};
+    $scope.errors ;
     $scope.searchMovie = function(){
-
+            $http.get('http://www.omdbapi.com/?t='+$scope.movieSearched.title+'&y='+$scope.movieSearched.year)
+                        .success(function(data,status,headers,config){
+                            $scope.results = data;
+                        })
+                          .error(function(error){
+                              console.log(error);
+                          });
     }
 
     $scope.reset = function(){
-
+        $scope.movieSearched = {};
+        $scope.results=  {};
 
     }
 
