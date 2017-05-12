f93431f306c89676c74551ef4e0fa09b5454b65c
ankush tripathi <ankushtripathi0996@yahoo.com>
2017-05-12 22:02:52 +0530

    final commit

--- js/controller/MoviesController.js
+++ js/controller/MoviesController.js
@@ -5,7 +5,18 @@ app.controller('SearchList',function($scope,$http){
     $scope.results = {};
     $scope.errors = "";
     $scope.hasError = false;
-    $scope.keys = [];
+    $scope.hasData = false;
+    $scope.movieData = function(){
+        var filtered = JSON.parse(JSON.stringify($scope.results));;
+        var keys = Object.keys(filtered);
+         for(var i=0;i<keys.length;i++){
+              if(filtered[keys[i]] === "N/A")
+                 delete filtered[keys[i]];
+             if(keys[i] == 'Ratings' || keys[i] === 'Plot' || keys[i] === 'Poster')
+                 delete filtered[keys[i]];
+         }
+        return filtered;
+    }
     $scope.searchMovie = function(){
         var url = 'http://www.omdbapi.com/?t='+$scope.movieSearched.title+'&y='+$scope.movieSearched.year;
             $scope.showLoader = true;
@@ -13,9 +24,11 @@ app.controller('SearchList',function($scope,$http){
                         .success(function(data,status,headers,config){
                             $scope.showLoader = false;
                              $scope.hasError = false;
+                             $scope.hasData = true;
                                 if(data.Response === "False")
                                    { 
                                        $scope.hasError = true;
+                                       $scope.hasData = false;
                                        $scope.errors = data.Error;
                                            $scope.movieSearched = {};
                                             $scope.results=  {};
@@ -31,6 +44,7 @@ app.controller('SearchList',function($scope,$http){
 
     $scope.reset = function(){
          $scope.hasError = false;
+         $scope.hasData = false;
         $scope.movieSearched = {};
         $scope.results=  {};
         $scope.errors = {};
