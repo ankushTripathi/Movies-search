b2990d9d6fe72f7442be6fd2b52f7456cfe2fa78
ankush tripathi <ankushtripathi0996@yahoo.com>
2017-05-12 11:35:08 +0530

    added loader , minor bug fix

--- js/controller/MoviesController.js
+++ js/controller/MoviesController.js
@@ -1,18 +1,16 @@
 app.controller('SearchList',function($scope,$http){
 
     $scope.movieSearched = {};
-    
+    $scope.showLoader = false;
     $scope.results = {};
     $scope.errors = {};
     $scope.searchMovie = function(){
         var url;
-        if("undefined" === typeof $scope.movieSearched.title)
-           $scope.errors = {"error": "Enter Movie Title"}; 
-        else
             url = 'http://www.omdbapi.com/?t='+$scope.movieSearched.title+'&y='+$scope.movieSearched.year;
-
+            $scope.showLoader = true;
             $http.get(url)
                         .success(function(data,status,headers,config){
+                            $scope.showLoader = false;
                             if(status === 200)
                              $scope.results = data;
                              else
